d9e414b6b0a365a4206e3c98ccfe9cda66aa66ce
Agung Dwi Sahputra <nezastsanjaya1@gmail.com>
2025-01-19 20:03:58 +0700

    Fix : Ketidaksesuaian tampilan pada success page Fix : Ketidaksesuaian tampilan pada order detail

--- resources/views/livewire/my-orders-detail-page.blade.php
+++ resources/views/livewire/my-orders-detail-page.blade.php
@@ -9,7 +9,7 @@
                 <div class="p-4 md:p-5 flex gap-x-4">
                     <div class="flex-shrink-0 flex justify-center items-center size-[46px] bg-gray-100 rounded-lg dark:bg-gray-800">
                         <svg class="flex-shrink-0 size-5 text-gray-600 dark:text-gray-400" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M16.0004 9V6C16.0004 3.79086 14.2095 2 12.0004 2C9.79123 2 8.00037 3.79086 8.00037 6V9M3.59237 10.352L2.99237 16.752C2.82178 18.5717 2.73648 19.4815 3.03842 20.1843C3.30367 20.8016 3.76849 21.3121 4.35839 21.6338C5.0299 22 5.94374 22 7.77142 22H16.2293C18.057 22 18.9708 22 19.6423 21.6338C20.2322 21.3121 20.6971 20.8016 20.9623 20.1843C21.2643 19.4815 21.179 18.5717 21.0084 16.752L20.4084 10.352C20.2643 8.81535 20.1923 8.04704 19.8467 7.46616C19.5424 6.95458 19.0927 6.54511 18.555 6.28984C17.9444 6 17.1727 6 15.6293 6L8.37142 6C6.82806 6 6.05638 6 5.44579 6.28984C4.90803 6.54511 4.45838 6.95458 4.15403 7.46616C3.80846 8.04704 3.73643 8.81534 3.59237 10.352Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M16.0004 9V6C16.0004 3.79086 14.2095 2 12.0004 2C9.79123 2 8.00037 3.79086 8.00037 6V9M3.59237 10.352L2.99237 16.752C2.82178 18.5717 2.73648 19.4815 3.03842 20.1843C3.30367 20.8016 3.76849 21.3121 4.35839 21.6338C5.0299 22 5.94374 22 7.77142 22H16.2293C18.057 22 18.9708 22 19.6423 21.6338C20.2322 21.3121 20.6971 20.8016 20.9623 20.1843C21.2643 19.4815 21.179 18.5717 21.0084 16.752L20.4084 10.352C20.2643 8.81535 20.1923 8.04704 19.8467 7.46616C19.5424 6.95458 19.0927 6.54511 18.555 6.28984C17.9444 6 17.1727 6 15.6293 6L8.37142 6C6.82806 6 6.05638 6 5.44579 6.28984C4.90803 6.54511 4.45838 6.95458 4.15403 7.46616C3.80846 8.04704 3.73643 8.81534 3.59237 10.352Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
                     </div>
 
@@ -32,7 +32,7 @@
                 <div class="p-4 md:p-5 flex gap-x-4">
                     <div class="flex-shrink-0 flex justify-center items-center size-[46px] bg-gray-100 rounded-lg dark:bg-gray-800">
                         <svg class="flex-shrink-0 size-5 text-gray-600 dark:text-gray-400" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M18 8L22 12M22 12L18 16M22 12H9M15 4.20404C13.7252 3.43827 12.2452 3 10.6667 3C5.8802 3 2 7.02944 2 12C2 16.9706 5.8802 21 10.6667 21C12.2452 21 13.7252 20.5617 15 19.796" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M18 8L22 12M22 12L18 16M22 12H9M15 4.20404C13.7252 3.43827 12.2452 3 10.6667 3C5.8802 3 2 7.02944 2 12C2 16.9706 5.8802 21 10.6667 21C12.2452 21 13.7252 20.5617 15 19.796" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                         </svg>
                     </div>
 
@@ -197,50 +197,56 @@
         <div class="flex flex-col md:flex-row gap-4 mt-4">
             <div class="md:w-3/4">
                 <div class="bg-white overflow-x-auto rounded-lg shadow-md p-6 mb-4">
-                    <table class="w-full">
-                        <thead>
+                    <table class="w-full min-w-full table-auto border-collapse border-spacing-0">
+                        <thead class="bg-gray-100 dark:bg-gray-800">
                             <tr>
-                                <th class="text-left font-semibold">Product</th>
-                                <th class="text-left font-semibold">Price</th>
-                                <th class="text-left font-semibold">Quantity</th>
-                                <th class="text-left font-semibold">Total</th>
+                                <th class="text-left font-semibold py-3 px-4 text-gray-700 dark:text-gray-300">Product</th>
+                                <th class="text-left font-semibold py-3 px-4 text-gray-700 dark:text-gray-300">Price</th>
+                                <th class="text-left font-semibold py-3 px-4 text-gray-700 dark:text-gray-300">Quantity</th>
+                                <th class="text-left font-semibold py-3 px-4 text-gray-700 dark:text-gray-300">Total</th>
                             </tr>
                         </thead>
                         <tbody>
-
                             @foreach ($order_items as $item)
-                                <tr wire:key="{{ $item->id }}">
-                                    <td class="py-4">
-                                        <div class="flex items-center">
-                                            <img class="h-16 w-16 mr-4" src="{{ url('storage', $item->product->images[0]) }}" alt="Image {{ $item->product->name }}">
-                                            <span class="font-semibold">{{ $item->product->name }}</span>
+                                <tr wire:key="{{ $item->id }}" class="border-b dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-700">
+                                    <td class="py-4 px-4">
+                                        <div class="flex items-center space-x-4">
+                                            <!-- Gambar -->
+                                            <img class="h-12 w-12 rounded-md object-cover hidden lg:block" src="{{ url('storage', $item->product->images[0]) }}" alt="Image {{ $item->product->name }}">
+                                            <!-- Nama Produk -->
+                                            <span class="font-semibold text-gray-800 dark:text-gray-200 break-words">
+                                                {{ $item->product->name }}
+                                            </span>
                                         </div>
                                     </td>
-                                    <td class="py-4">Rp. {{ number_format($item->unit_amount, 0, ',', '.') }}</td>
-                                    <td class="py-4">
-                                        <span class="text-center w-8">{{ $item->quantity }}</span>
+                                    <td class="py-4 px-4 text-gray-700 dark:text-gray-300 whitespace-nowrap">
+                                        Rp. {{ number_format($item->unit_amount, 0, ',', '.') }}
                                     </td>
-                                    <td class="py-4">Rp. {{ number_format($item->total_amount, 0, ',', '.') }}</td>
+                                    <td class="py-4 px-4 text-center text-gray-800 dark:text-gray-200">
+                                        {{ $item->quantity }}
+                                    </td>
+                                    <td class="py-4 px-4 text-gray-700 dark:text-gray-300 whitespace-nowrap">
+                                        Rp. {{ number_format($item->total_amount, 0, ',', '.') }}
+                                    </td>
                                 </tr>
                             @endforeach
-
                         </tbody>
                     </table>
                 </div>
 
                 <div class="bg-white overflow-x-auto rounded-lg shadow-md p-6 mb-4">
-                    <h1 class="font-3xl font-bold text-slate-500 mb-3">Shipping Address</h1>
-                    <div class="flex justify-between items-center">
+                    <h1 class="text-3xl font-bold text-slate-500 mb-3">Shipping Address</h1>
+                    <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
                         <div>
-                            <p>{{ $order->address->address . ', ' . $order->address->city . ', ' . $order->address->state . ', ' . $order->address->zip_code }}</p>
+                            <p class="text-gray-800 dark:text-gray-300">{{ $order->address->address . ', ' . $order->address->city . ', ' . $order->address->state . ', ' . $order->address->zip_code }}</p>
                         </div>
                         <div>
-                            <p class="font-semibold">Phone:</p>
-                            <p>{{ $order->address->phone }}</p>
+                            <p class="font-semibold text-gray-800 dark:text-gray-300">Phone:</p>
+                            <p class="text-gray-700 dark:text-gray-400">{{ $order->address->phone }}</p>
                         </div>
                         <div>
-                            <p class="font-semibold">Email:</p>
-                            <p>{{ $order->address->email }}</p>
+                            <p class="font-semibold text-gray-800 dark:text-gray-300">Email:</p>
+                            <p class="text-gray-700 dark:text-gray-400">{{ $order->address->email }}</p>
                         </div>
                     </div>
                 </div>
